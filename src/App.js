c2bcc5da4336bc6fdd069f57962457d199c1d89d
leo-ronan <57088324+leo-ronan@users.noreply.github.com>
2021-01-28 06:53:37 -0500

    Update App.js
    required app.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from './components/navbar.js';
 import Home from "./components/Home.js";
+import "./App.css";
 
 function App() {
   return (
